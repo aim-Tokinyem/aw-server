b83eb24c01805782791f140a1f58fcda52048cf9
aim-Tokinyem <aimtokinyem0311@gmail.com>
2023-08-14 15:07:07 +0700

    update status server

--- aw_server/__about__.py
+++ aw_server/__about__.py
@@ -16,7 +16,7 @@ projectpath = srcpath.parent
 bundlepath = projectpath.parent  # the ActivityWatch bundle repo, in some circumstances
 
 # This line set by script when run (metaprogramming)
-__version__ = "v0.12.3b6.dev+d75d1e6"
+__version__ = "v0.12.3b6.dev+1647a48"
 
 
 def get_rev():
